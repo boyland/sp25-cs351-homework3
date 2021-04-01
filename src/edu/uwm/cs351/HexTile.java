9b2107d40390d3e99e96f40cc1b6800c053323bf
14014 <14014@LAPTOP-24JRIQF7.attlocal.net>
2021-04-01 10:14:47 -0500

    added equals method

--- src/edu/uwm/cs351/HexTile.java
+++ src/edu/uwm/cs351/HexTile.java
@@ -9,6 +9,34 @@ import java.awt.Polygon;
  */
 public class HexTile {
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((location == null) ? 0 : location.hashCode());
+		result = prime * result + ((terrain == null) ? 0 : terrain.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HexTile other = (HexTile) obj;
+		if (location == null) {
+			if (other.location != null)
+				return false;
+		} else if (!location.equals(other.location))
+			return false;
+		if (terrain != other.terrain)
+			return false;
+		return true;
+	}
+
 	public static final int WIDTH = 50; // this value could be changed
 	
 	private final Terrain terrain;
