9b8ab4e34f3696c0ffb512e1072b641750ea1c83
John Boyland <boyland@uwm.edu>
2025-01-21 07:25:33 -0600

    added etc items

---
+++ etc/config.mk
@@ -0,0 +1,20 @@
+HW1= ${REPOS}/homework1
+
+INSTALLSRC = \
+	src/TestHexTileCollection.java \
+	src/TestInternals.java \
+	src/UnlockTest.java \
+	src/edu/uwm/cs351/FormatException.java \
+	src/edu/uwm/cs351/HexTile.java \
+	src/edu/uwm/cs351/HexCoordinate.java \
+	test/sample.hex
+
+INSTALLSKEL = \
+	src/edu/uwm/cs351/HexTileCollection.java \
+	src/Demo.java
+
+INSTALLDIR = src src/edu src/edu/uwm src/edu/uwm/cs351 lib test
+
+INSTALLJAR = -C ${HW1}/bin edu/uwm/cs351 \
+             ${LOCKEDTESTS} ${RANDOMTESTS}
+
