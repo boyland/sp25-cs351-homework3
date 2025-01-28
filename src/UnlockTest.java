75884d5b5d5600924e9968d1c4d7d5720ceeec52
John Boyland <boyland@uwm.edu>
2025-01-28 09:03:37 -0600

    UnlockTest

--- src/UnlockTest.java
+++ src/UnlockTest.java
@@ -2,7 +2,7 @@ import edu.uwm.cs.junit.LockedTestCase;
 
 public class UnlockTest {
 	public static void main(String[] args){
-		unlock("TestBallCollection");
+		unlock("TestHexTileCollection");
 	}
 	
 	private static void unlock(String classname){
